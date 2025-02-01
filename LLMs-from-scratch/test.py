2912f0c1e2057c1dfc9954e62f882544d8d85980
WangKai1123 <wk563006423@163.com>
2025-02-01 15:09:11 +0800

    new feature:增加手写attention

---
+++ LLMs-from-scratch/test.py
@@ -0,0 +1,10 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-mini-4k-instruct",
+    device_map="cuda",
+    torch_dtype="auto",
+    trust_remote_code=True,
+)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
